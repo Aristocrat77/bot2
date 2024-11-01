c926fa25adeef00ca6f93b7d1e4584c5e28e813c
Lenovo Win-11 <feskovvlad@gmail.com>
2024-11-01 15:49:52 +0300

    add file2

--- bot2.py
+++ bot2.py
@@ -7,7 +7,7 @@ from telebot import types
 
 @botTimeWeb.message_handler(commands=['start'])
 def startBot(message):
-  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь расскажу немного о нашей компании?"
+  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!"
   markup = types.InlineKeyboardMarkup()
   button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
   markup.add(button_yes)
