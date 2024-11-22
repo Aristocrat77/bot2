4ddc8b68d99d81f74bb2eabc9610754c241894ed
Aristocrat77 <feskovvlad@gmail.com>
2024-11-22 17:23:50 +0300

    new cod

--- bot2.py
+++ bot2.py
@@ -1,66 +1,56 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup
+from telebot import types
 
 bot = telebot.TeleBot('7721748157:AAH5NlI6XHxw2U5pJDywdlES1H41R9bF6J0')
 
-from telebot import types
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    main = Main(bot)
+    main.start(message)
 
 
-@bot.message_handler(content_types=['text'])
-def welcome(massage):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    btn1 = types.InlineKeyboardButton("Мужчина", callback_data="1")
-    btn2 = types.InlineKeyboardButton("Женщина", callback_data="2")
-    markup.add(btn1, btn2)
-    sd = bot.send_message(massage.chat.id, 'Чтобы узнать БЖУ укажите свой пол',
-                     reply_markup=markup, parse_mode='html')
-    print(sd)
-    bot.register_next_step_handler(sd, v_handler)
 
+class Main:
+    def __init__ (self, bot):
+        self.height = None
+        self.weight = None
+        self.bot = bot
+        print('Main started')
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call, self=None):
-    try:
-        if call.data == '1':
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text="Вы мужчина теперь введите свой рост")
-        elif call.data == '2':
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text="Вы женщина теперь введите свой рост")
-        else:
-            bot.answer_callback_query(callback_query_id=call.id, text="Неизвестная команда")
-    except Exception as e:
-        print(repr(e))
+    def start(self, message):
+        raw_height = self.bot.send_message(message.chat.id, 'привет я бот! Введи рост.')
+        self.bot.register_next_step_handler(raw_height, self.height_handler)
 
+    def height_handler(self, message):
+        try:
+            height = int(message.text)
+            self.height = height
 
-def v_handler(pm):
-    try:
-        v = int(pm.text)
-        sent_msg = bot.send_message(pm.chat.id, f"Ваш рост {v}. Теперь введите свой вес")
-        print(v)
-        bot.register_next_step_handler(sent_msg, weight_handler, v)
-    except ValueError:
-        bot.send_message(pm.chat.id, 'Вы ввели некорректное число!')
+            raw_weight = self.bot.send_message(message.chat.id, f"Ваш рост {height}. Теперь введите свой вес")
+            self.bot.register_next_step_handler(raw_weight, self.weight_handler)
+        except ValueError:
+            self.bot.send_message(message.chat.id, 'Вы ввели некорректное число!')
 
+    def weight_handler(self, message):
+        try:
+            weight = int(message.text)
+            self.weight = weight
+            raw_age = self.bot.send_message(message.chat.id, f"Ваш вес {weight}. Введите возраст")
+            self.bot.register_next_step_handler(raw_age, self.age_handler)
+        except ValueError:
+            self.bot.send_message(message.chat.id, 'Вы ввели некорректное число!')
 
-def weight_handler(pm, v):
-    try:
-        weight = int(pm.text)
-        sent_msg2 = bot.send_message(pm.chat.id, f"Ваш вес {weight}. Введите возраст")
-        print(weight)
-        bot.register_next_step_handler(sent_msg2, age_handler, v)
-    except ValueError:
-        bot.send_message(pm.chat.id, 'Вы ввели некорректное число!')
-
-
-def age_handler(pm, q):
-    try:
-        age = int(pm.text)
-        bot.send_message(pm.chat.id, f"Ваш возраст {age}.")
-        bot.send_message(pm.chat.id, f" жопа ")
-        print(age)
-    except ValueError:
-        bot.send_message(pm.chat.id, 'Вы ввели некорректное число!')
+    def age_handler(self, message):
+        try:
+            age = int(message.text)
+            self.bot.send_message(message.chat.id, f"Ваш возраст {age}, {self.height}, {self.weight} .")
+        except ValueError:
+            self.bot.send_message(message.chat.id, 'Вы ввели некорректное число!')
 
 
 bot.polling(none_stop=True, interval=0)
+
+
+
